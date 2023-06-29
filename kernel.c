181e86d2398c38d99cadba7542724663ad7d33b7
as9-ali <Ahmed17.Ali@live.uwe.ac.uk>
2023-06-29 04:49:57 -0700

    Added scrolling function

--- kernel.c
+++ kernel.c
@@ -61,6 +61,16 @@ size_t terminal_row;
 size_t terminal_column;
 uint8_t terminal_color;
 uint16_t* terminal_buffer;
+
+void terminal_sft() {
+  for (size_t y = 0; y < VGA_HEIGHT - 1; y++) { 
+    for (size_t x = 0; x < VGA_WIDTH; x++) {
+      const size_t index = y * VGA_WIDTH + x;
+      const size_t new_index = (y + 1) * VGA_WIDTH + x;
+      terminal_buffer[index] = terminal_buffer[new_index];
+    }
+  }
+}
  
 void terminal_initialize() {
   terminal_row = 0;
@@ -74,6 +84,7 @@ void terminal_initialize() {
     }
   }
 }
+
  
 void terminal_setcolor(uint8_t color) {
   terminal_color = color;
@@ -85,24 +96,35 @@ void terminal_putentryat(char c, uint8_t color, size_t x, size_t y) {
 }
 
 void terminal_putchar(char c) {
-  terminal_putentryat(c, terminal_color, terminal_column, terminal_row);
-  if (++terminal_column == VGA_WIDTH) {
+	
+  // Handle new line, by checking if the character is a newline
+  if (c=='\n') {
+    ++terminal_row;
     terminal_column = 0;
-    if (++terminal_row == VGA_HEIGHT) {
-      terminal_row = 0;
+    if (terminal_row == VGA_HEIGHT) {
+      terminal_sft();
+      terminal_row--;
     }
   }
+
+  // If the character is not a newline
+  else {
+    terminal_putentryat(c, terminal_color, terminal_column, terminal_row);
+    if (++terminal_column == VGA_WIDTH) {
+      terminal_column = 0;
+      if (++terminal_row == VGA_HEIGHT) {
+       terminal_sft();
+       terminal_row--;
+      }
+    }
+  }
+
 }
  
 void terminal_writestring(const char* data) {
   size_t datalen = strlen(data);
   for (size_t i = 0; i < datalen; i++) {
-    if (data[i] == '\n') {
-      terminal_column = 0;
-      terminal_row++;
-    } else {
-      terminal_putchar(data[i]);
-    }
+    terminal_putchar(data[i]);
   }
 }
 
